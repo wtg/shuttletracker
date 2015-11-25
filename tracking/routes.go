548d80dfb773bbaa0612a919d68c01b2e3ad63d0
gbprz <perez283@gmail.com>
2015-11-24 22:20:18 -0500

    Refactor Route struct to contain slice of coordinates

--- tracking/routes.go
+++ tracking/routes.go
@@ -3,10 +3,17 @@ package tracking
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
 
 	"gopkg.in/mgo.v2/bson"
 )
+
+// Coord represents a single lat/lng point used to draw routes
+type Coord struct {
+  Lat     float64       `json:"lat"     bson:"lat"`
+  Lng     float64       `json:"lng"     bson:"lng"`
+}
 
 // Route represents a set of coordinates to draw a path on our tracking map
 type Route struct {
@@ -18,7 +25,7 @@ type Route struct {
 	Enabled     bool          `json:"enabled,string" bson:"enabled"`
 	Color       string        `json:"color"          bson:"color"`
 	Width       int           `json:"width,string"   bson:"width"`
-	Coords      string        `json:"coords"         bson:"coords"`
+	Coords      []Coord       `json:"coords"         bson:"coords"`
 	Created     time.Time     `json:"created"        bson:"created"`
 	Updated     time.Time     `json:"updated"        bson:"updated"`
 }
@@ -65,12 +72,43 @@ func (App *App) StopsHandler(w http.ResponseWriter, r *http.Request) {
 // RoutesCreateHandler adds a new route to the database
 func (App *App) RoutesCreateHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a new route object using request fields
-	route := Route{}
-	err := json.NewDecoder(r.Body).Decode(&route)
+	var routeData map[string]string
+	var coordsData []map[string]float64
+	// Decode route details 
+	err := json.NewDecoder(r.Body).Decode(&routeData)
 	// Error handling
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+	// Unmarshal route coordinates
+	err = json.Unmarshal([]byte(routeData["coords"]), &coordsData)
+	// Error handling
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	// Create a Coord from each set of input coordinates
+	coords := []Coord{}
+	for _, c := range coordsData {
+		coord := Coord{c["lat"], c["lng"]}
+		coords = append(coords, coord)
+	}
+	// Type conversions 
+	enabled,_ := strconv.ParseBool(routeData["enabled"])
+	width,_ := strconv.Atoi(routeData["width"])
+	currentTime := time.Now()
+	// Create a new route 
+	route := Route { 
+		bson.NewObjectId(),
+		routeData["name"],
+		routeData["description"],
+		routeData["startTime"],
+		routeData["endTime"],
+		enabled,
+	  routeData["color"],
+		width,
+		coords,
+		currentTime,
+		currentTime} 
 	// Store new route under routes collection
 	err = App.Routes.Insert(&route)
 	// Error handling
