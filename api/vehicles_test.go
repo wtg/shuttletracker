d4944f34a1d39471555dcea8de72433899aa5b6a
Sidney Kochman <sidney@kochman.org>
2017-07-11 22:13:15 -0400

    first test yay so cute

---
+++ api/vehicles_test.go
@@ -0,0 +1,24 @@
+package api
+
+import "testing"
+
+func TestCardinalDirection(t *testing.T) {
+	table := [][]string{
+		{"0", "North"},
+		{"45", "North-East"},
+		{"90", "East"},
+		{"135", "South-East"},
+		{"180", "South"},
+		{"225", "South-West"},
+		{"270", "West"},
+		{"315", "North-West"},
+	}
+
+	for _, testCase := range table {
+		direction := CardinalDirection(&testCase[0])
+		expected := testCase[1]
+		if direction != expected {
+			t.Errorf("Got %v, expected %v.", direction, expected)
+		}
+	}
+}
