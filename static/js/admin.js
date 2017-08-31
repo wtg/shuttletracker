afb598d22a0c94b9ee740ac3f5fc64f2b2e308c0
jlyon1 <joeylyon1@gmail.com>
2017-08-31 13:49:04 -0400

    working branch

--- static/js/admin.js
+++ static/js/admin.js
@@ -7,7 +7,12 @@ var Admin = {
 
   populateStops: function(data){
     console.log(data);
-
+    for(var i = 0; i < data.length; i ++){
+      $("#Stops").append("<div class = 'stop' id='123'><b> Stop 1</b><p> Stop Information here</p><button class = 'rbtn'><i class='fa fa-trash' aria-hidden='true'></i></button><button class = 'rbtn'><i class='fa fa-pencil' aria-hidden='true'></i></button></div>");
+      if(i % 2 == 0 && i != 0){
+        $("#Stops").append("<br style='clear:both;'/>")
+      }
+    }
   }
 
 
