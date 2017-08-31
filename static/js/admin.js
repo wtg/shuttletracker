5704c4d89a85ac910fd27c8aa054eaeec6c661d0
jlyon1 <joeylyon1@gmail.com>
2017-08-31 13:03:28 -0400

    changed some buttons for sidney

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,8 +1,18 @@
 var Admin = {
 
+  grabStops: function(){
+    $.get( "http://127.0.0.1:8080/stops", Admin.populateStops);
+
+  },
+
   populateStops: function(data){
-    
+    console.log(data);
+
   }
 
 
 }
+
+$(document).ready(function(){
+  Admin.grabStops();
+});
