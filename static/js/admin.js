ed9d71badd607f5d6b69c81d5ea6c0169a72220c
jlyon1 <joeylyon1@gmail.com>
2017-09-15 09:03:23 -0400

    begin implementing route creation interface

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,5 +1,6 @@
 var Admin = {
   RoutesMap: null,
+  ShuttleRoutes: [],
   /*
   <div class = "route-description-box">
     <span class = "emphasis">name:</span><span class ="content"> West Campus</span><br>
@@ -12,9 +13,66 @@ var Admin = {
 
   </div>
   */
+
+  updateRoutes: function(data){
+    var updatedRoute = [];
+    for(var i = 0; i < data.length; i ++){
+      var points = [];
+      for(var j = 0; j < data[i].coords.length; j ++){
+        points.push(new L.LatLng(data[i].coords[j].lat,data[i].coords[j].lng));
+      }
+      var polylineOptions = {
+        color: data[i].color,
+        weight: 3,
+        opacity: 1,
+      };
+      if(data[i].width === 0){
+        polylineOptions.dashArray = '10,10';
+      }
+
+      var polyline = new L.Polyline(points, polylineOptions);
+
+      var r ={
+        name: data[i].name,
+        id: data[i].id,
+        description: data[i].description,
+        color: data[i].color,
+        created: data[i].created,
+        enabled: data[i].enabled,
+        stops: data[i].stopsid,
+        start_time: data[i].startTime,
+        end_time: data[i].endTime,
+        points: points,
+        line: polyline
+      };
+
+      updatedRoute.push(r);
+
+    }
+
+    Admin.ShuttleRoutes = updatedRoute;
+    Admin.drawRoutes();
+
+  },
+
+
+  drawRoutes: function(){
+
+    var polylineOptions = {
+      color: 'blue',
+      weight: 1,
+      opacity: 1
+    };
+    for(i = 0; i < Admin.ShuttleRoutes.length; i ++){
+      Admin.RoutesMap.addLayer(Admin.ShuttleRoutes[i].line);
+    }
+
+  },
+
   populateRoutesPanel: function(data){
     console.log(data);
     $(".routePanel").html("");
+    //Admin.updateRoutes(data);
     if(data == null){
 
     }else{
@@ -27,10 +85,12 @@ var Admin = {
           url: '/routes/' + $(this).attr("routeId"),
           type: 'DELETE',
           success: function(result) {
-            $.get( "/routes", Admin.populateRoutesPanel);
+            Admin.populateRoutesPanel(data);
+            //$.get( "/routes", Admin.populateRoutesPanel);
           }
         });
       });
+
     }
   },
 
@@ -43,7 +103,7 @@ var Admin = {
     box += "<span class = 'emphasis'>color:</span><span class='content'>" + routeInfo.color + "</span><br>";
     box += "<span class = 'emphasis'>time:</span><span class='content'>lel</span><br>";
     box += "<span class = 'emphasis'>id:</span><span class='content'>"+ routeInfo.id + "</span><br>";
-    box += "<div style='float: right;width:auto;'><button class='button cbutton'>edit</button><button id='delete' routeId="+routeInfo.id +" class='button cbutton deleteroute'>delete</button></div><br></div>"
+    box += "<div style='float: right;width:auto;'><button class='button cbutton'>change</button><button id='delete' routeId="+routeInfo.id +" class='button cbutton deleteroute'>delete</button></div><br></div>"
     $(".routePanel").append(box);
 
   },
@@ -68,9 +128,30 @@ var Admin = {
       minZoom: 14
     }).addTo(Admin.RoutesMap);
 
+    var drawnRoute = new L.FeatureGroup();
+    Admin.RoutesMap.addLayer(drawnRoute);
+    var drawControl = new L.Control.Draw({
+      edit: {
+        featureGroup: drawnRoute
+      },
+      draw: {
+        polygon: false,
+        marker: false,
+        circle: false,
+        rectangle: false,
+      },
+    });
+    Admin.RoutesMap.addControl(drawControl);
+    Admin.RoutesMap.on(L.Draw.Event.CREATED, function (e) {
+      var type = e.layerType,
+      layer = e.layer;
+
+      drawnRoute.addLayer(layer);
+    });
   }
 
 };
+
 
 $(document).ready(function(){
   Admin.initMap();
