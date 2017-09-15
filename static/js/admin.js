e8e133aacd8dc4112b3c2f9f6fd9754b8759db79
jlyon1 <joeylyon1@gmail.com>
2017-09-14 21:27:06 -0400

    populate routes in admin interface

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,8 +1,36 @@
 var Admin = {
   RoutesMap: null,
+  /*
+  <div class = "route-description-box">
+    <span class = "emphasis">name:</span><span class ="content"> West Campus</span><br>
+    <span class = "emphasis">description:</span><span class ="content"> The west campus Red Hawk shuttle will make stops at the Student Union Horseshoe, Sage Ave & Troy Building crosswalks, Blitman Residence Commons, 6th Avenue and City Station, Polytechnic Residence Commons, and 15th Street and College Avenue. This shuttle route will have a yellow indicator labeled "WEST" on the front of the shuttle.</span><br>
+    <span class = "emphasis">enabled:</span><span class="content">true</span><br>
+    <span class = "emphasis">color:</span><span class="content">#fff</span><br>
+    <span class = "emphasis">time:</span><span class="content">idek</span><br>
+    <span class = "emphasis">id:</span><span class="content">13445213a</span><br>
+    <div style="float: right;"><a class="button">edit</a><a class="button">delete</a></div><br>
 
-  buildRouteBox: function(){
+  </div>
+  */
+  populateRoutesPanel: function(data){
+    console.log(data);
+    for(var i = 0; i < data.length; i ++){
+      console.log(data[i]);
+      Admin.buildRouteBox(data[i]);
+    }
+  },
 
+  buildRouteBox: function(routeInfo){
+    var box = "";
+    box += "<div id = " + routeInfo.id +" class = 'route-description-box'>";
+    box += "<span class = 'emphasis'>name:</span><span class ='content'> " + routeInfo.name + "</span><br>";
+    box += "<span class = 'emphasis'>description:</span><span class ='content'>" + routeInfo.description + "</span><br>"
+    box += "<span class = 'emphasis'>enabled:</span><span class='content'>"+routeInfo.enabled + "</span><br>";
+    box += "<span class = 'emphasis'>color:</span><span class='content'>" + routeInfo.color + "</span><br>";
+    box += "<span class = 'emphasis'>time:</span><span class='content'>lel</span><br>";
+    box += "<span class = 'emphasis'>id:</span><span class='content'>"+ routeInfo.id + "</span><br>";
+    box += "<div style='float: right;'><a class='button'>edit</a><a class='button'>delete</a></div><br>"
+    $(".routePanel").append(box);
   },
 
   initMap: function(){
@@ -27,4 +55,7 @@ var Admin = {
 
 $(document).ready(function(){
   Admin.initMap();
+  $.get( "/routes", Admin.populateRoutesPanel);
+
+
 });
