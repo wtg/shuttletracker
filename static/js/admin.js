da45eaea7f146e7f9d17de1c8d74a3fdb3ffbb1b
jlyon1 <joeylyon1@gmail.com>
2017-09-14 21:14:41 -0400

    new branch updating route interface

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,3 +1,30 @@
 var Admin = {
+  RoutesMap: null,
+
+  buildRouteBox: function(){
+
+  },
+
+  initMap: function(){
+    Admin.RoutesMap = L.map('mapid', {
+        zoomControl: false,
+        attributionControl: false
+    });
+    Admin.RoutesMap.setView([42.728172, -73.678803], 15.3);
+    Admin.RoutesMap.addControl(L.control.attribution({
+        position: 'bottomright',
+        prefix: ''
+    }));
+    L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}{r}.png', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+      maxZoom: 17,
+      minZoom: 14
+    }).addTo(Admin.RoutesMap);
+
+  }
 
 };
+
+$(document).ready(function(){
+  Admin.initMap();
+});
