df22741e9820b74a58194f96471362fd7b280111
Jonathan Caicedo <Jonathan@jcaicedo.com>
2017-10-12 21:34:36 +0000

    Switch to Stamen's secure CDN

--- static/js/admin.js
+++ static/js/admin.js
@@ -303,7 +303,7 @@ var Admin = {
         prefix: ''
       }));
 
-      L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}{r}.png', {
+      L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
         minZoom: 13
       }).addTo(Admin.StopsMap);
--- static/js/app.js
+++ static/js/app.js
@@ -20,7 +20,7 @@ var App ={
         position: 'bottomright',
         prefix: ''
     }));
-    L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png', {
       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
       maxZoom: 17,
       minZoom: 14
