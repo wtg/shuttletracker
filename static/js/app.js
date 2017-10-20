47842570d5770e81861e1aba2a12d74316b20b04
Jonathan Elias Caicedo <Jonathan@jcaicedo.com>
2017-10-19 22:25:24 -0400

    Add support for showing user location (#80)
    * Update README.md with clarifications; add make-admin.sh script
    
    * Add showing basic user location
    
    * Add showing basic user location
    
    * Fix style issues in static/js/app.js
    
    * Fix missing semicolon
    
    * Grab user location after grabVehicles interval is called

--- static/js/app.js
+++ static/js/app.js
@@ -180,6 +180,35 @@ var App ={
     App.grabVehicleInfo();
 
   },
+  
+  showUserLocation: function(){
+    var userIcon = L.icon({
+      iconUrl: 'static/images/stop.png',
+      
+      iconSize:     [12, 12], // size of the icon
+      iconAnchor:   [6, 6], // point of the icon which will correspond to marker's location
+      shadowAnchor: [6, 6],  // the same for the shadow
+      popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+    });
+    
+     if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else { 
+        console.log("Geolocation is either not supported by this browser, or geolocation permissions were not given by the user.");
+      }
+    
+    function showPosition (position) {
+      var locationMarker = {
+            name: "Current Location",
+            marker: L.marker([position.coords.latitude, position.coords.longitude], {
+                icon: userIcon,
+                zIndexOffset: 1000
+            }),
+      };
+      locationMarker.marker.bindPopup(locationMarker.name);
+      locationMarker.marker.addTo(App.ShuttleMap);
+    }
+  },
 
   stopClicked: function(e){
   },
@@ -220,4 +249,5 @@ $(document).ready(function(){
   App.initMap();
   App.grabStops();
   var a = setInterval(App.grabVehicles, 1000);
+  App.showUserLocation();
 });
