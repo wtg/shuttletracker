31245be95314f2906a47799983b635811ba457bd
Sidney Kochman <sidney@kochman.org>
2018-03-02 09:57:30 -0500

    Fix admin messages form

--- static/js/messages.js
+++ static/js/messages.js
@@ -2,14 +2,26 @@
 Vue.component('message-panel', {
   template:
   `<div class ="column">
-    <div class="tile is-parent">
-      <div class="tile box is-parent">
-        <div class="tile notification" v-bind:class="{'is-danger':fail, 'is-white':!fail}">
-          <div class="container">
-          <p>Message: </p> <input class="textbox" style="width:75%;" v-model="message"></input> Characters: {{message.length}}
-          <br>
-          <input type="checkbox" v-model="display">Enabled</input><br>
-          <button class="button" @click="send">Submit</button>
+      <div class="box">
+        <div class="notification" v-bind:class="{'is-danger':fail, 'is-white':!fail}">
+          <div class="field">
+            <label class="label">Message</label>
+            <div class="control">
+              <input class="textbox input" v-model="message">
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <label class="checkbox">
+                <input type="checkbox" v-model="display">
+                Enabled
+              </label>
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <button class="button" @click="send">Submit</button>
+            </div>
           </div>
         </div>
       </div>
