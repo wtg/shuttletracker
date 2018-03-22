a62badad6ca72c63a94dc796e1e5b7699881ff10
Sidney Kochman <sidney@kochman.org>
2018-03-22 01:25:11 -0400

    go fmt

--- api/etag.go
+++ api/etag.go
@@ -1,43 +1,43 @@
 package api
 
 import (
-    "net/http"
-    "bytes"
-    "crypto/sha1"
-    "hash"
-    "io"
-    "fmt"
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"hash"
+	"io"
+	"net/http"
 )
 
 type etagResponseWriter struct {
-    http.ResponseWriter
-    buf bytes.Buffer
-    hash hash.Hash
-    w io.Writer
+	http.ResponseWriter
+	buf  bytes.Buffer
+	hash hash.Hash
+	w    io.Writer
 }
 
 func (e *etagResponseWriter) Write(p []byte) (int, error) {
-    return e.w.Write(p)
+	return e.w.Write(p)
 }
 
 func etag(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        ew := &etagResponseWriter{
-            ResponseWriter: w,
-            buf: bytes.Buffer{},
-            hash: sha1.New(),
-        }
-        ew.w = io.MultiWriter(&ew.buf, ew.hash)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ew := &etagResponseWriter{
+			ResponseWriter: w,
+			buf:            bytes.Buffer{},
+			hash:           sha1.New(),
+		}
+		ew.w = io.MultiWriter(&ew.buf, ew.hash)
 
-        next.ServeHTTP(ew, r)
+		next.ServeHTTP(ew, r)
 
-        sum := fmt.Sprintf("%x", ew.hash.Sum(nil))
-        w.Header().Set("ETag", sum)
+		sum := fmt.Sprintf("%x", ew.hash.Sum(nil))
+		w.Header().Set("ETag", sum)
 
-        if r.Header.Get("If-None-Match") == sum {
-            w.WriteHeader(304)
-        } else {
-            ew.buf.WriteTo(w)
-        }
-    })
+		if r.Header.Get("If-None-Match") == sum {
+			w.WriteHeader(304)
+		} else {
+			ew.buf.WriteTo(w)
+		}
+	})
 }
