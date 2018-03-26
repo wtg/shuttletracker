18fffe658807cace3271ef35a18874918856c06e
Sidney Kochman <sidney@kochman.org>
2018-03-25 20:04:16 -0400

    Add static file tests

--- api/api_test.go
+++ api/api_test.go
@@ -2,9 +2,70 @@ package api
 
 import (
 	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+
+	"github.com/wtg/shuttletracker/database"
 )
+
+func TestStatic(t *testing.T) {
+	type testCase struct {
+		method string
+		path   string
+	}
+	cases := []testCase{
+		{
+			method: "GET",
+			path:   "/",
+		},
+		{
+			method: "GET",
+			path:   "/static/css/application.css",
+		},
+		{
+			method: "GET",
+			path:   "/static/js/frontend.js",
+		},
+	}
+
+	// Go tests are run from the package dir, but our static files are one level higher
+	os.Chdir("..")
+
+	cfg := Config{}
+	db := &database.Mock{}
+
+	api, err := New(cfg, db)
+	if err != nil {
+		t.Errorf("got error '%s', expected nil", err)
+		return
+	}
+
+	server := httptest.NewServer(api.handler)
+	defer server.Close()
+	client := server.Client()
+
+	for _, c := range cases {
+		url := server.URL + c.path
+		req, err := http.NewRequest(c.method, url, nil)
+		if err != nil {
+			t.Errorf("unable to create HTTP request: %s", err)
+			continue
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Errorf("unable to do request: %s", err)
+			continue
+		}
+
+		if resp.StatusCode != 200 {
+			t.Logf("%+v", req)
+			t.Logf("%+v", resp)
+			t.Errorf("%s %s returned status code %d, expected 200", c.method, url, resp.StatusCode)
+		}
+	}
+}
 
 func TestWriteJSON(t *testing.T) {
 	type testCase struct {
