wtg/shuttletracker

commit 450ffc94f0f1c9cba5ed57f22746ed57e5d28653

Generate messages client side (#142)

static/js/frontend.js
@@ -293,11 +293,11 @@ Vue.component('shuttle-map',{
 
     },
 
-    grabVehicles: function(){
+    grabVehicles: function () {
       $.get( "/updates", this.updateVehicles).fail(function(){vehicleUpdateSuccess = false;});
     },
 
-    updateVehicles: function(data){
+    updateVehicles: function (data) {
       window.d = new Date();
       var hours = window.d.getHours();
       hours = checkTime(hours);
@@ -346,7 +346,7 @@ Vue.component('shuttle-map',{
             data[j].color = "#FFF";
           }
 
-          if(this.ShuttlesArray[data[j].vehicleID] === undefined){
+          if (this.ShuttlesArray[data[j].vehicleID] === undefined){
             shuttleIcon.options.iconUrl = this.getShuttleIcon(data[j].color);
             this.ShuttlesArray[data[j].vehicleID] = {
               data: data[j],
@@ -358,17 +358,64 @@ Vue.component('shuttle-map',{
               message: ""
             };
             this.ShuttlesArray[data[j].vehicleID].marker.addTo(this.ShuttleMap);
-          }else{
+          } else {
             shuttleIcon.options.iconUrl = this.getShuttleIcon(data[j].color);
+            this.ShuttlesArray[data[j].vehicleID].data = data[j];
             this.ShuttlesArray[data[j].vehicleID].marker.setIcon(shuttleIcon);
             this.ShuttlesArray[data[j].vehicleID].marker.setLatLng([data[j].lat,data[j].lng]);
             this.ShuttlesArray[data[j].vehicleID].marker.setRotationAngle(parseInt(data[j].heading)-45);
           }
         }
       }
-      this.ShuttleUpdateCounter ++;
-      this.grabVehicleInfo();
 
+      this.setVehicleMessages();
+
+      this.ShuttleUpdateCounter++;
+    },
+
+    cardinalDirection: function(heading) {
+      if (heading >= 22.5 && heading < 67.5) {
+        return "northeast";
+      } else if (heading >= 67.5 && heading < 112.5) {
+        return "east";
+      } else if (heading >= 112.5 && heading < 157.5) {
+        return "southeast";
+      } else if (heading >= 157.5 && heading < 202.5) {
+        return "south";
+      } else if (heading >= 202.5 && heading < 247.5) {
+        return "southwest";
+      } else if (heading >= 247.5 && heading < 292.5) {
+        return "west";
+      } else if (heading >= 292.5 && heading < 337.5) {
+        return "northwest";
+      }
+      return "north";
+    },
+
+    setVehicleMessages: function () {
+      const v = this;
+      $.get("/vehicles", function (data) {
+        vehicleUpdateSuccess = true;
+        let idToName = {};
+        for (let i = 0; i < data.length; i++) {
+          idToName[data[i].vehicleID] = data[i].vehicleName;
+        }
+
+        for (const key in v.ShuttlesArray) {
+
+          const update = v.ShuttlesArray[key].data;
+          const speed = Math.round(update.speed * 100) / 100;
+          const date = new Date(update.created);
+          const direction = v.cardinalDirection(parseFloat(update.heading));
+
+          const message = `<b>${idToName[key]}</b><br>` +
+          `Traveling ${direction} at ${speed} mph<br>` +
+          `as of ${date.toLocaleTimeString()}`;
+          v.ShuttlesArray[key].marker.bindPopup(message);
+        }
+      }).fail(function() {
+        vehicleUpdateSuccess = false;
+      });
     },
 
     showUserLocation: function(map){
@@ -403,43 +450,6 @@ Vue.component('shuttle-map',{
 
     stopClicked: function(e){
     },
-
-    grabVehicleInfo: function(){
-      $.get( "/vehicles", this.grabMessages).fail(function(){vehicleUpdateSuccess = false;});
-
-    },
-    updateMessages: function(){
-      for(var key in this.ShuttlesArray){
-        for(var messageKey in this.ShuttleMessages){
-          if(key == messageKey && this.ShuttlesArray[key] !== null){
-            this.ShuttlesArray[key].marker.bindPopup(this.ShuttleMessages[messageKey]);
-          }
-        }
-      }
-
-    },
-
-    grabMessages: function(data){
-      vehicleUpdateSuccess = true;
-      var nameToId = {};
-      for(var i = 0; i < data.length; i ++){
-        nameToId[data[i].vehicleName] = data[i].vehicleID;
-      }
-      var el = this;
-      $.get( "/updates/message", function(data){
-        vehicleMessageSuccess = true;
-        for(var i = 0 ; i < data.length; i ++){
-
-          var start_pos = data[i].indexOf('>') + 1;
-          var end_pos = data[i].indexOf('<',start_pos);
-          el.ShuttleMessages[nameToId[data[i].substring(start_pos,end_pos)]] = data[i];
-
-        }
-        el.updateMessages();
-      }).fail(function(){vehicleMessageSuccess = false;});
-
-    },
-
   }
 });
 

api/api.go
@@ -74,7 +74,6 @@ func New(cfg Config, db database.Database) (*API, error) {
 	// Updates
 	r.Route("/updates", func(r chi.Router) {
 		r.Get("/", api.UpdatesHandler)
-		r.Get("/message", api.UpdateMessageHandler)
 	})
 
 	// Admin message

api/vehicles.go
@@ -2,9 +2,7 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -131,70 +129,4 @@ func (api *API) UpdatesHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Convert updates to JSON
 	WriteJSON(w, updates) // it's good to take some REST in our server :)
-}
-
-// UpdateMessageHandler generates a message about an update for a vehicle
-func (api *API) UpdateMessageHandler(w http.ResponseWriter, r *http.Request) {
-	// For each vehicle/update, store message as a string
-	var messages []string
-	var message string
-
-	// Query all Vehicles
-	vehicles, err := api.db.GetVehicles()
-	// Handle errors
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	// Find recent updates and generate message
-	for _, vehicle := range vehicles {
-		// find 10 most recent records
-		update, err := api.db.GetLastUpdateForVehicle(vehicle.VehicleID)
-		if err == nil {
-			// Use first 4 char substring of update.Speed
-			speed := update.Speed
-			if len(speed) > 4 {
-				speed = speed[0:4]
-			}
-
-			// Convert last updated time to local timezone
-			loc, err := time.LoadLocation("America/New_York")
-			if err != nil {
-				log.WithError(err).Error("Could not load time zone information.")
-				continue
-			}
-			lastUpdate := update.Created.In(loc).Format("3:04:05pm")
-
-			message = fmt.Sprintf("<b>%s</b><br/>Traveling %s at<br/> %s mph as of %s", vehicle.VehicleName, CardinalDirection(&update.Heading), speed, lastUpdate)
-			messages = append(messages, message)
-		}
-	}
-	// Convert to JSON
-	WriteJSON(w, messages)
-}
-
-// CardinalDirection returns the cardinal direction of a vehicle's heading.
-func CardinalDirection(h *string) string {
-	heading, err := strconv.ParseFloat(*h, 64)
-	if err != nil {
-		log.WithError(err).Error("Unable to parse float")
-		return "North"
-	}
-	switch {
-	case (heading >= 22.5 && heading < 67.5):
-		return "North-East"
-	case (heading >= 67.5 && heading < 112.5):
-		return "East"
-	case (heading >= 112.5 && heading < 157.5):
-		return "South-East"
-	case (heading >= 157.5 && heading < 202.5):
-		return "South"
-	case (heading >= 202.5 && heading < 247.5):
-		return "South-West"
-	case (heading >= 247.5 && heading < 292.5):
-		return "West"
-	case (heading >= 292.5 && heading < 337.5):
-		return "North-West"
-	default:
-		return "North"
-	}
 }

api/vehicles_test.go
@@ -1,25 +1,0 @@
-package api
-
-import "testing"
-
-func TestCardinalDirection(t *testing.T) {
-	table := [][]string{
-		{"0", "North"},
-		{"45", "North-East"},
-		{"90", "East"},
-		{"135", "South-East"},
-		{"180", "South"},
-		{"225", "South-West"},
-		{"270", "West"},
-		{"315", "North-West"},
-		{"this isn't a direction lol", "North"},
-	}
-
-	for _, testCase := range table {
-		direction := CardinalDirection(&testCase[0])
-		expected := testCase[1]
-		if direction != expected {
-			t.Errorf("Got %v, expected %v.", direction, expected)
-		}
-	}
-}

commit b8aa033dd60df7eaf80f8710622a337f59a21c9c

Fix potential live indicator bug (#144)
* Fix potential live indicator bug introduced in 450ffc9

static/js/frontend.js
@@ -3,8 +3,7 @@ var live = true;
 var partial = false;
 var routeSuccess = true;
 var stopsSuccess = true;
-var vehicleUpdateSuccess = true;
-var vehicleMessageSuccess = true;
+var vehicleSuccess = true;
 var lastUpdateTime = "";
 
 var d = new Date();
@@ -30,8 +29,8 @@ Vue.component('live-indicator',{
   },
   methods: {
     update: function(){
-      live = routeSuccess && vehicleUpdateSuccess && vehicleMessageSuccess;
-      partial = routeSuccess || vehicleUpdateSuccess;
+      live = routeSuccess && vehicleSuccess;
+      partial = routeSuccess || vehicleSuccess;
       if(live === false){
         this.text = window.lastUpdateTime;
         this.liveStyle.width="auto";
@@ -294,7 +293,7 @@ Vue.component('shuttle-map',{
     },
 
     grabVehicles: function () {
-      $.get( "/updates", this.updateVehicles).fail(function(){vehicleUpdateSuccess = false;});
+      $.get( "/updates", this.updateVehicles).fail(function(){vehicleSuccess = false;});
     },
 
     updateVehicles: function (data) {
@@ -307,7 +306,7 @@ Vue.component('shuttle-map',{
       secs = checkTime(secs);
 
       window.lastUpdateTime = (hours) + ":" + (mins) + ":" + (secs);
-      vehicleUpdateSuccess = true;
+      vehicleSuccess = true;
       var shuttleIcon = L.icon({
         iconUrl: this.getShuttleIcon("#FFF"),
 
@@ -395,7 +394,7 @@ Vue.component('shuttle-map',{
     setVehicleMessages: function () {
       const v = this;
       $.get("/vehicles", function (data) {
-        vehicleUpdateSuccess = true;
+        vehicleSuccess = true;
         let idToName = {};
         for (let i = 0; i < data.length; i++) {
           idToName[data[i].vehicleID] = data[i].vehicleName;
@@ -414,7 +413,7 @@ Vue.component('shuttle-map',{
           v.ShuttlesArray[key].marker.bindPopup(message);
         }
       }).fail(function() {
-        vehicleUpdateSuccess = false;
+        vehicleSuccess = false;
       });
     },
 