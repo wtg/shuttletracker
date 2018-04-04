3ff4ecc94c5c3c85d3701041c574ad9d91e1c6e3
Sidney Kochman <sidney@kochman.org>
2018-04-04 15:32:55 -0400

    Add test for returned vehicles

--- api/vehicles_test.go
+++ api/vehicles_test.go
@@ -50,3 +50,60 @@ func TestVehiclesHandlerNoVehicles(t *testing.T) {
 	vs.AssertExpectations(t)
 	vs.AssertNumberOfCalls(t, "Vehicles", 1)
 }
+
+func TestVehiclesHandlerTwoVehicles(t *testing.T) {
+	vs := &mock.VehicleService{}
+	vehicles := []*shuttletracker.Vehicle{
+		{
+			Name:    "Vehicle 1",
+			Enabled: true,
+		},
+		{
+			Name:    "Vehicle 2",
+			Enabled: true,
+		},
+	}
+	vs.On("Vehicles").Return(vehicles, nil)
+
+	api := API{
+		vs: vs,
+	}
+
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil {
+		t.Errorf("unable to create HTTP request: %s", err)
+		return
+	}
+
+	api.VehiclesHandler(w, req)
+	resp := w.Result()
+
+	if resp.StatusCode != 200 {
+		t.Errorf("got status code %d, expected 200", resp.StatusCode)
+	}
+	if resp.Header.Get("Content-Type") != "application/json" {
+		t.Errorf("got Content-Type \"%s\", expected \"application/json\"", resp.Header.Get("Content-Type"))
+	}
+
+	var returnedVehicles []*shuttletracker.Vehicle
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&returnedVehicles)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+		return
+	}
+
+	if len(returnedVehicles) != 2 {
+		t.Errorf("got %d vehicles, expected 2", len(returnedVehicles))
+	}
+
+	for i := range vehicles {
+		if *vehicles[i] != *returnedVehicles[i] {
+			t.Errorf("got different vehicles at index %d: %+v expected %+v", i, returnedVehicles[i], vehicles[i])
+		}
+	}
+
+	vs.AssertExpectations(t)
+	vs.AssertNumberOfCalls(t, "Vehicles", 1)
+}
