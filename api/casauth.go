302a2b28b417be372dae7e05ff7736611a11d995
jlyon1 <joeylyon1@gmail.com>
2018-04-04 11:27:04 -0400

    Fix an error

--- api/casauth.go
+++ api/casauth.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/wtg/shuttletracker/database"
+	"github.com/wtg/shuttletracker/log"
 	"gopkg.in/cas.v2"
 	"net/http"
 	"strings"
@@ -21,11 +22,16 @@ func (cli *casClient) casauth(next http.Handler) http.Handler {
 	return cli.cas.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		if !cas.IsAuthenticated(r) {
-			w.Write([]byte("redirecting to cas;"))
+			_, err := w.Write([]byte("redirecting to cas;"))
+			if err != nil {
+				log.WithError(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
 			cas.RedirectToLogin(w, r)
 		} else {
 			auth, err := cli.db.UserExists(strings.ToLower(cas.Username(r)))
 			if err != nil {
+				log.WithError(err)
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				auth = false
 			}
