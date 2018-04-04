dfd94bc322542f2ad34a22c8391cfe73d0281e88
Sidney Kochman <sidney@kochman.org>
2018-04-04 16:14:47 -0400

    Add test for creating vehicle

--- api/vehicles_test.go
+++ api/vehicles_test.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -106,4 +108,52 @@ func TestVehiclesHandlerTwoVehicles(t *testing.T) {
 
 	vs.AssertExpectations(t)
 	vs.AssertNumberOfCalls(t, "Vehicles", 1)
+}
+
+func TestVehiclesCreateHandler(t *testing.T) {
+	vs := &mock.VehicleService{}
+	vehicle := &shuttletracker.Vehicle{
+		Name:      "Vehicle 2",
+		Enabled:   true,
+		TrackerID: 2,
+	}
+	vs.On("CreateVehicle", vehicle).Return(nil)
+
+	api := API{
+		vs: vs,
+	}
+
+	body := &bytes.Buffer{}
+	enc := json.NewEncoder(body)
+	err := enc.Encode(vehicle)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", "", body)
+	if err != nil {
+		t.Errorf("unable to create HTTP request: %s", err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+	api.VehiclesCreateHandler(w, req)
+	resp := w.Result()
+
+	if resp.StatusCode != 200 {
+		t.Errorf("got status code %d, expected 200", resp.StatusCode)
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+		return
+	}
+	if len(respBody) != 0 {
+		t.Errorf("got body length %d, expected 0", len(respBody))
+	}
+
+	vs.AssertExpectations(t)
+	vs.AssertNumberOfCalls(t, "CreateVehicle", 1)
 }
