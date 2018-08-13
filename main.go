e6de321b950b3761aca1b36cd5eb1e46611d4e53
Sidney Kochman <sidney@kochman.org>
2018-08-13 12:08:53 -0400

    Remove empty file

--- main.go
+++
@@ -1,3 +0,0 @@
-// Package shuttletracker displays the positions of RPI's shuttles on a map.
-// See https://shuttles.rpi.edu and https://github.com/wtg/shuttletracker for more information.
-package shuttletracker
