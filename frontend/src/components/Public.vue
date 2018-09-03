1c259717f2ec186f7ee1b3e9a1b91dcc9572d891
jlyon1 <joeylyon1@gmail.com>
2018-09-02 22:36:43 -0400

    add map

--- frontend/src/components/Public.vue
+++ frontend/src/components/Public.vue
@@ -9,9 +9,7 @@
           <img src="~../assets/wtg.svg" />
         </div>
     </div>
-    {{vehicles}}
-    {{routes}}
-    {{stops}}
+    <div id="mymap"></div>
 </div>
 </template>
 
@@ -22,6 +20,7 @@ import Vehicle from '../structures/vehicle';
 import Route from '../structures/route';
 import Stop from '../structures/stop';
 import dropdown from './dropdown.vue';
+import * as L from 'leaflet';
 
 export default Vue.extend({
   name: 'Public',
@@ -41,6 +40,21 @@ export default Vue.extend({
     a.GrabVehicles().then((data: Vehicle[]) => this.vehicles = data);
     a.GrabRoutes().then((data: Route[]) => this.routes = data);
     a.GrabStops().then((data: Stop[]) => this.stops = data);
+    const ShuttleMap = L.map('mymap', {
+      zoomControl: false,
+      attributionControl: false, // hide Leaflet
+    });
+    ShuttleMap.setView([42.728172, -73.678803], 15.3);
+    // show attribution without Leaflet
+    ShuttleMap.addControl(L.control.attribution({
+        position: 'bottomright',
+        prefix: '',
+    }));
+    L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+      maxZoom: 17,
+      minZoom: 14,
+    }).addTo(ShuttleMap);
   },
   components: {
     dropdown,
@@ -49,6 +63,12 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
+
+#mymap{
+  height: 100%;
+  position: relative;
+}
+
 .titleBar {
     height: 34px;
     float: none;
