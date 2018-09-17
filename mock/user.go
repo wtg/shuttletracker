940ccbae52621ca28eebcb70623d297cd1216d30
Sidney Kochman <sidney@kochman.org>
2018-09-17 18:04:41 -0400

    Update mock UserService

--- mock/user.go
+++ mock/user.go
@@ -2,6 +2,7 @@ package mock
 
 import (
 	"github.com/stretchr/testify/mock"
+	"github.com/wtg/shuttletracker"
 )
 
 // UserService implements a mock of shuttletracker.UserService.
@@ -13,4 +14,10 @@ type UserService struct {
 func (us *UserService) UserExists(username string) (bool, error) {
 	args := us.Called(username)
 	return args.Bool(0), args.Error(1)
+}
+
+// Users gets all Users.
+func (us *UserService) Users() ([]*shuttletracker.User, error) {
+	args := us.Called()
+	return args.Get(0).([]*shuttletracker.User), args.Error(1)
 }
