d2e9a7d9b71220ca73a4116b98df3806d602358c
Sidney Kochman <sidney@kochman.org>
2018-10-13 13:18:33 -0400

    Add schedule modification test

--- postgres/route_test.go
+++ postgres/route_test.go
@@ -87,3 +87,84 @@ func TestCreateSchedule(t *testing.T) {
 		t.Error("route is not active")
 	}
 }
+
+// nolint: gocyclo
+func TestModifySchedule(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	pg := setUpPostgres(t)
+	defer tearDownPostgres(t)
+
+	route := &shuttletracker.Route{
+		Name:     "Test Route",
+		Schedule: shuttletracker.RouteSchedule{},
+	}
+	err := pg.CreateRoute(route)
+	if err != nil {
+		t.Fatalf("unable to create Route: %s", err)
+	}
+
+	// make sure the schedule is the correct size and route is active
+	if len(route.Schedule) != 0 {
+		t.Errorf("wrong schedule length: %d", len(route.Schedule))
+	}
+	if !route.Active {
+		t.Error("route is not active")
+	}
+
+	// modify the route schedule and check again
+	route.Schedule = shuttletracker.RouteSchedule{
+		shuttletracker.RouteActiveInterval{
+			StartDay:  time.Sunday,
+			StartTime: time.Date(0, 1, 0, 0, 0, 0, 0, time.UTC),
+			EndDay:    time.Saturday,
+			EndTime:   time.Date(0, 1, 0, 23, 59, 59, 0, time.UTC),
+		},
+	}
+	err = pg.ModifyRoute(route)
+	if err != nil {
+		t.Fatalf("unable to modify Route: %s", err)
+	}
+	if len(route.Schedule) != 1 {
+		t.Errorf("wrong schedule length: %d", len(route.Schedule))
+	}
+	if !route.Active {
+		t.Error("route is not active")
+	}
+
+	route, err = pg.Route(route.ID)
+	if err != nil {
+		t.Fatalf("unable to get Route: %s", err)
+	}
+	if len(route.Schedule) != 1 {
+		t.Errorf("wrong schedule length: %d", len(route.Schedule))
+	}
+	if !route.Active {
+		t.Error("route is not active")
+	}
+
+	// delete the route schedule and check again
+	route.Schedule = shuttletracker.RouteSchedule{}
+	err = pg.ModifyRoute(route)
+	if err != nil {
+		t.Fatalf("unable to modify Route: %s", err)
+	}
+	if len(route.Schedule) != 0 {
+		t.Errorf("wrong schedule length: %d", len(route.Schedule))
+	}
+	if !route.Active {
+		t.Error("route is not active")
+	}
+
+	route, err = pg.Route(route.ID)
+	if err != nil {
+		t.Fatalf("unable to get Route: %s", err)
+	}
+	if len(route.Schedule) != 0 {
+		t.Errorf("wrong schedule length: %d", len(route.Schedule))
+	}
+	if !route.Active {
+		t.Error("route is not active")
+	}
+}
