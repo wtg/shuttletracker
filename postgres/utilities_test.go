f936435efb18713d00f5815c0738b684f8330aa6
Sidney Kochman <sidney@kochman.org>
2018-10-26 00:16:27 -0400

    Admins management command (#165)
    * Move to command-based package layout
    
    * Add admins list command
    
    * Don't use log package in user-facing commands
    
    * Update mock UserService
    
    * Allow adding users from command line
    
    * Allow removing users from command line
    
    * Remove example database folder
    
    * remove dummy data
    
    * Update readme
    
    * Fix gometalinter issues
    
    * Re-add codecov config
    
    * Remove custom codecov config
    
    * Upload coverage to codecov before preparing to cache
    
    * fix comment
    
    * Add postgres UserService tests
    
    * remove useless vendored package entry
    
    * remove unused vendored package
    
    * Add another argument check

---
+++ postgres/utilities_test.go
@@ -0,0 +1,44 @@
+// Utilities for testing the postgres package.
+package postgres
+
+import (
+	"database/sql"
+	"testing"
+)
+
+const url = "postgres://postgres@localhost/shuttletracker_test?sslmode=disable"
+
+func setUpPostgres(t *testing.T) *Postgres {
+	// determine if this database is clean
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		t.Fatalf("unable to open database: %s", err)
+	}
+	row := db.QueryRow("select count(*) from information_schema.tables where table_schema = 'public';")
+	var numTables int
+	err = row.Scan(&numTables)
+	if err != nil {
+		t.Fatalf("unable to scan: %s", err)
+	}
+	if numTables != 0 {
+		t.Fatalf("database is not empty")
+	}
+
+	pg, err := New(Config{URL: url})
+	if err != nil {
+		t.Fatalf("unable to create Postgres: %s", err)
+	}
+
+	return pg
+}
+
+func tearDownPostgres(t *testing.T) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		t.Fatalf("unable to open database: %s", err)
+	}
+	_, err = db.Exec("drop schema public cascade; create schema public authorization postgres;")
+	if err != nil {
+		t.Fatalf("unable to clear database: %s", err)
+	}
+}
