6a12821624c88ad1fad07cf56d637189014288bb
Joseph Lyon <joeylyon1@gmail.com>
2018-11-05 14:31:42 -0500

    add a few things

---
+++ frontend/src/components/admin/routesAdmin.vue
@@ -0,0 +1,12 @@
+<template>
+    <div style="margin-top: 50px;" class="container">
+        ROUTES HEY
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: "routes"
+    
+})
+</script>
