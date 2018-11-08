1a383372d1fb62479bad0c5ae4c40ab21dfad07f
Frank Vega-Aguirre <fvaguirre@gmail.com>
2018-11-08 16:04:52 -0500

    trying to get history endpoint running

--- api/api.go
+++ api/api.go
@@ -73,6 +73,11 @@ func New(cfg Config, ms shuttletracker.ModelService, msg shuttletracker.MessageS
 		r.Get("/", api.UpdatesHandler)
 	})
 
+	//Hisory
+	r.Route("/history", func(r chi.Router) {
+		r.Get("/", api.HistoryHandler)
+		})
+
 	// Admin message
 	r.Route("/adminMessage", func(r chi.Router) {
 		r.Get("/", api.AdminMessageHandler)
