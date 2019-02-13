e8b38db1c9e8dea9d00dde37826bb97d25d7b269
Sidney Kochman <sidney@kochman.org>
2019-02-13 13:06:58 -0500

    reconnecting websocket

--- frontend/src/fusion.ts
+++ frontend/src/fusion.ts
@@ -1,11 +1,47 @@
+// SocketManager wraps a WebSocket in order to provide guarantees about
+// reliability, reconnections, retries, etc.
+class SocketManager {
+    ws: WebSocket
+    url: string
+
+    constructor(url: string) {
+        this.url = url;
+        this.ws = this.createSocket();
+    }
+
+    createSocket() {
+        const ws = new WebSocket(this.url);
+        ws.onopen = event => {
+            console.log("socket connected", event);
+        };
+        ws.onerror = event => {
+            console.log("socket error", event);
+        };
+        ws.onclose = event => {
+            console.log("socket closed", event);
+            this.ws = this.createSocket();
+        };
+        return ws;
+    }
+
+    send(msg: string) {
+        this.ws.send(msg);
+    }
+}
+
 export default class Fusion {
+    ws: SocketManager;
+
     constructor() {
         console.log("Fusion created.");
-        const ws = new WebSocket("ws://localhost:8080/fusion");
-        ws.onopen = (event) => {
-            ws.send("hey fusion");
-        };
 
+        this.ws = new SocketManager("ws://localhost:8080/fusion");
+
+        // this.startSocket();
+        this.startGeolocation();
+    }
+
+    startGeolocation() {
         const options = { enableHighAccuracy: true, maximumAge: 0 };
         navigator.geolocation.watchPosition(
             position => {
@@ -15,7 +51,7 @@ export default class Fusion {
                     heading: position.coords.heading,
                     speed: position.coords.speed
                 };
-                ws.send(JSON.stringify(data));
+                this.ws.send(JSON.stringify(data));
             },
             error => {
                 console.log("could not get position", error);
