1b9e94883f262a41cf3e68e707da65fd5ab30c07
Joseph Lyon <joeylyon1@gmail.com>
2019-02-25 00:40:24 -0500

    update text on settings page

--- frontend/src/components/settings.vue
+++ frontend/src/components/settings.vue
@@ -4,7 +4,7 @@
     <hr>
     <div class="field">
       <b-switch v-model="fusionPositionEnabled">Send position updates</b-switch>
-      <p class="help">Use your location to help make Shuttle Tracker more accurate.</p>
+      <p class="help">Use your location to help make Shuttle Tracker more accurate. When enabled your location is collected anonymously while Shuttle Tracker is open.</p>
     </div>
     <div class="field">
       <b-switch v-model="busButtonEnabled">Bus button</b-switch>
@@ -12,7 +12,7 @@
     </div>
 
     <!-- <h3 class="title is-3">About</h3> -->
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About and privacy policy</router-link>
   </div>
 </template>
 
