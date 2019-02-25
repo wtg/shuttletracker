b06f364094f762235612034359beb344263bb9af
Sidney Kochman <sidney@kochman.org>
2019-02-25 00:07:29 -0500

    hide some scroll bars

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,18 +21,19 @@ export default Vue.extend({
   },
 });
 </script>
-
+ddciwhci
 <style lang="scss">
 #app {
   width: 100%;
   height: 100%;
+  overflow: hidden;
 }
 #view-wrapper {
   height: calc(100% - 40px);
   box-sizing: border-box;
 }
 #router-view {
-  overflow: scroll;
+  overflow-y: auto;
   -webkit-overflow-scrolling: touch;
   height: 100%;
 }
