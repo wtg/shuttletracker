f56a51bb02d368b90810b22779eec71092435a01
Sidney Kochman <sidney@kochman.org>
2019-02-28 11:58:58 -0500

    use vue-analytics with Vue Router integration

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,9 @@ import 'typeface-open-sans';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 
+// @ts-ignore
+import VueAnalytics from 'vue-analytics';
+
 // only include the specific fontawesome icons that we use
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
 import '@fortawesome/fontawesome-free/css/solid.css';
@@ -13,8 +16,16 @@ import store from '@/store';
 import router from '@/index';
 import '@/assets/styles.scss';
 
-Vue.use(Buefy);
 Vue.config.productionTip = false;
+
+Vue.use(Buefy);
+Vue.use(VueAnalytics, {
+  id: 'UA-28203673-6',
+  autoTracking: {
+    exception: true,
+  },
+  router,
+});
 
 /**
  * Declare the main Vue instance with components and vuex store.
