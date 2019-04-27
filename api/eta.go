63b0313e61f7956e9f35ae941e23b3e8a3218cb3
Yutong Gao <lisa_gao_2333@163.com>
2019-04-26 21:40:17 -0400

    based on the comments on my pull request to wtg/shuttle tracker, make changes

--- api/eta.go
+++ api/eta.go
@@ -1,55 +1,115 @@
 package api
 
-import (
-	_"encoding/json"
-	"net/http"
-	_"strconv"
+import (   //blank import import those below; we can use factored import declaration import two more things
+	_ "encoding/json"
 	"fmt"
-	_"github.com/wtg/shuttletracker"
+	"github.com/wtg/shuttletracker"
 	"github.com/wtg/shuttletracker/log"
-    "reflect"
-    "math"
+	"math"
+	"net/http"
+	_ "reflect"
+	_ "strconv"
 )
 
-// EtaHandler finds all of the routes in the database
+// etas at /eta
+
 func (api *API) EtaHandler(w http.ResponseWriter, r *http.Request) {
+	//get routes and stops info from DB
 	routes, err := api.ms.Routes()
 	if err != nil {
 		log.WithError(err).Error("unable to get eta")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Println(reflect.TypeOf(routes))
-	// fmt.Println(len(routes))
-	// points:=routes[1].Points[1].Latitude
-	distances:= make([]float64, len(routes)) 
-	for i:=0; i<len(routes);i++{
-	 	// fmt.Println(routes[i].ID,routes[i].Name)
-	 	points:=routes[i].Points
-	 	var dis float64=0
-	 	var lat,lon float64
-	 	for j:=0; j<len(points);j++{
-	 		if j==0{
-		 		lat=points[j].Latitude/360*2*math.Pi
-		 		lon=points[j].Longitude/360*2*math.Pi
-		 	}else{
-		 		lat1:=points[j].Latitude/360*2*math.Pi
-		 		lon1:=points[j].Longitude/360*2*math.Pi
-		 		dlon:=lon1-lon
-		 		dlat:=lat1-lat
-		 		a:=math.Pow(math.Sin(dlat/2),2) + math.Cos(lat1)*math.Cos(lat)*math.Pow(math.Sin(dlon/2),2)
-		 		c := 2*math.Asin(math.Sqrt(a))
-		 		dis+=c*6371/1.6
-		 		lat=lat1
-		 		lon=lon1
-		 	}
-	 		// fmt.Println(lat,lon)
-	 	}
-	 	fmt.Println(dis)
-	 	distances[i]=dis
+	stops, err := api.ms.Stops()
+	if err != nil {
+		log.WithError(err).Error("unable to get stops")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	fmt.Println()
-	// fmt.Println(distances)
-	// fmt.Println(reflect.TypeOf(points))
+
+	//Since we have not data feed offline, use random data to test
+	tmpcar :=shuttletracker.Point{42.73166,-73.68559} //A random car location in west campus
+	tmproute:=routes[0]                                // use only west campus route
+
+
+	StopIndices :=GetStopsIndices(routes[2].Points,stops)
+
+	index:=FindAffinity(tmproute.Points, tmpcar)
+
+	distances:= make(map[string]float64)
+
+	// get the distance from the car to all the stops
+	for k, v := range StopIndices {
+		fmt.Println(v)
+		distances[k]=GetDistance(tmproute.Points,index,v)
+	}
+
+	// get the distance from Blitman to union and back
+	distances["BlitmanToUnion"]=GetDistance(tmproute.Points,18,276)
+	distances["UnionToBlitman"]=GetDistance(tmproute.Points,276,18)
+
+	//get the total distance of all the routes
+	for i:=0; i<len(routes);i++{
+		points:=routes[i].Points
+		str:=routes[i].Name+"'s total distances"
+		distances[str]=GetDistance(points,0,len(points)-1)
+	}
+
 	WriteJSON(w, distances)
+}
+
+//calculate the distance between two points on the route
+
+func GetDistance(points []shuttletracker.Point,start int,end int) float64{
+	if end==start{
+		return 0.0
+	}
+	if end<start{
+		return GetDistance(points,start,len(points)-1)+GetDistance(points,0,end)
+	}
+	var dis float64=0
+	var lat,lon float64
+	for j:=start; j<=end;j++{
+		if j==start{
+			lat=points[j].Latitude/360*2*math.Pi
+			lon= points[j].Longitude / 360 * 2 * math.Pi
+		}else{
+			lat1:=points[j].Latitude/360*2*math.Pi
+			lon1:=points[j].Longitude/360*2*math.Pi
+			dlon:=lon1-lon
+			dlat:=lat1-lat
+			a:=math.Pow(math.Sin(dlat/2),2) + math.Cos(lat1)*math.Cos(lat)*math.Pow(math.Sin(dlon/2),2)
+			c := 2*math.Asin(math.Sqrt(a))
+			dis+=c*6371/1.6
+			lat=lat1
+			lon=lon1
+		}
+	}
+	return dis
+}
+
+// find the index of the closest point of p on the route points
+
+func FindAffinity(points []shuttletracker.Point, p shuttletracker.Point) int{
+	diff:=math.Pow((points[0].Latitude-p.Latitude),2)+math.Pow((points[0].Longitude-p.Longitude),2)
+	min:=diff
+	minIndex:=0
+	for i:=1;i<len(points);i++{
+		diff=math.Pow((points[i].Latitude-p.Latitude),2)+math.Pow((points[i].Longitude-p.Longitude),2)
+		if diff<min{
+			min=diff
+			minIndex=i
+		}
+	}
+	return minIndex
+}
+
+// get the index of closest point of each stop in stops
+func GetStopsIndices(points []shuttletracker.Point, stops []*shuttletracker.Stop) map[string]int{
+	indices:=make(map[string]int)
+	for i:=0; i<len(stops);i++{
+		indices[*stops[i].Name]=FindAffinity(points,shuttletracker.Point{stops[i].Latitude,stops[i].Longitude})
+	}
+	return indices
 }
