0e1e8dcbc0e9cea0a97094dfd6c2728df92ab3c7
Zachary Beshara <32651240+beshaz@users.noreply.github.com>
2019-08-08 14:23:51 -0400

    Links in admin messages (#246)
    * Fixed issue # 189
    
    Added support for a link within an admin message
    
    * Add warning for URL but no text
    
    * Fix formatting causing travisci error

--- frontend/src/structures/adminMessageUpdate.ts
+++ frontend/src/structures/adminMessageUpdate.ts
@@ -5,12 +5,14 @@ export default class AdminMessageUpdate {
     public enabled: boolean;
     public created: Date;
     public updated: Date;
+    public link: string;
 
-    constructor(message: string, enabled: boolean, created: Date, updated: Date) {
+    constructor(message: string, enabled: boolean, created: Date, updated: Date, link: string) {
         this.message = message;
         this.enabled = enabled;
         this.created = created;
         this.updated = updated;
+        this.link = link;
 
     }
 
