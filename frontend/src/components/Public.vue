3ab8c1732d2b3ce5d71a986017ce4e2f09338076
prashadr2 <prashadr2@gmail.com>
2019-10-01 15:56:31 -0400

    changed emoji

--- frontend/src/components/Public.vue
+++ frontend/src/components/Public.vue
@@ -229,7 +229,7 @@ export default Vue.extend({
       }
       this.userShuttleidCount ++;
       const busIcon = L.divIcon({
-        html: `<span class="shuttleusericon shuttleusericon` + String(this.userShuttleidCount) +  `" >🚐</span>`,
+        html: `<span class="shuttleusericon shuttleusericon` + String(this.userShuttleidCount) +  `" >🚀</span>`,
 
         iconSize: [20, 20], // size of the icon
         iconAnchor: [10, 10], // point of the icon which will correspond to marker's location
