cc28c290cdb41f7f3cafaf3808e9d0514b7fb6b8
Joseph Om <joeom516@gmail.com>
2019-10-08 18:34:38 -0400

    Set up routing to /tvpanel and add vue scaffold for the tvpanel

---
+++ frontend/src/components/tvpanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    TV PANEL
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
+
+<style lang='scss' scoped></style>
