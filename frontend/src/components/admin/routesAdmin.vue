b37e4069ed6c21213af4d11a12e8d59cfe566e0a
farrer <farrer@rpi.edu>
2019-10-08 16:52:50 -0400

    Added button to select file to import and non-functional submit

--- frontend/src/components/admin/routesAdmin.vue
+++ frontend/src/components/admin/routesAdmin.vue
@@ -41,6 +41,14 @@
                   <th></th>
                   <th><button @click="" class="button is-success">Export</button></th>
                 </tr>
+                <div class="container">
+                  <div class="large-12 medium-12 small-12 cell">
+                    <label>File
+                      <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+                    </label>
+                      <button v-on:click="submitFile()">Submit</button>
+                  </div>
+                </div>
             </tbody>
         </table>
     </div>
