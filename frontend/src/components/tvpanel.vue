43351a2059402ad7493830801679a7ba1f505aea
Joseph Om <joeom516@gmail.com>
2019-10-08 22:21:36 -0400

    TV-Panel: Add Map component functionality to the tvpanel

--- frontend/src/components/tvpanel.vue
+++ frontend/src/components/tvpanel.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
-    TV PANEL
+    <!-- Map Component -->
+    <Map class="map"/>
   </div>
 </template>
 
 <script lang='ts'>
 import Vue from 'vue';
-export default Vue.extend({});
+import Map from './tvpanel/Map.vue';
+import Fusion from '@/fusion';
+export default Vue.extend({
+  name: 'tvpanel',
+  data() {
+  return{
+    fusion: new Fusion(),
+  }as {
+      fusion: Fusion;
+  };
+},
+mounted() {
+  this.fusion.start();
+},
+  components: {
+    Map,
+  },
+});
 </script>
 
-<style lang='scss' scoped></style>
+<style lang='scss'>
+
+#view-wrapper {
+  height: 100%;
+}
+
+.map {
+  position: relative;
+  height: 100%;
+  width: 70%;
+}
+</style>
