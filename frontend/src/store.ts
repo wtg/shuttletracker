3c97998660a25c537a38e4c76fb002fce8d240fe
ruchika-singh <55224151+ruchika-singh@users.noreply.github.com>
2019-10-18 18:42:34 -0400

    Added bus button choices (#262)
    * first commit to busbutton fork
    
    * is this work?
    
    * added testfile
    
    * attempt 2
    
    * try add test file
    
    * final test
    
    * another test
    
    * another nother test
    
    * Hi
    
    * hii
    
    * update
    
    * master update
    
    * changed emoji
    
    * test
    
    * Adding bus button choice attempt
    
    * compiler errors
    
    * trailing whitespace
    
    * html menu
    
    * html edit
    
    * drop down menu complete
    
    * More emoji and message
    
    * Edited button to show which one was chosen
    
    * Put button on same line as message
    
    * deleted files
    
    * Added back test.sh
    
    * fixes
    
    * added conf.json.sample file
    
    * fixed newline
    
    * fixed code on go.mod
    
    * fixed permission

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -30,6 +30,7 @@ const store: StoreOptions<StoreState> = {
       busButtonEnabled: false,
       etasEnabled: false,
       fusionPositionEnabled: true,
+      busButtonChoice: '🚌',
     },
     geolocationDenied: false,
     fusionConnected: undefined,
@@ -113,6 +114,10 @@ const store: StoreOptions<StoreState> = {
     },
     setSettingsBusButtonEnabled(state, value: boolean) {
       state.settings.busButtonEnabled = value;
+      localStorage.setItem('st_settings', JSON.stringify(state.settings));
+    },
+    setSettingsBusButtonChoice(state, value: string) {
+      state.settings.busButtonChoice = value;
       localStorage.setItem('st_settings', JSON.stringify(state.settings));
     },
     setSettingsETAsEnabled(state, value: boolean) {
