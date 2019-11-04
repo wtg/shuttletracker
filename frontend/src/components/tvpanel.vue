3d4af033e202cfb5c83de06570c23957b9c17ff0
Erica Bertolozzi <ericabertolozzi@gmail.com>
2019-11-03 23:12:45 -0500

    TV-Panel: Added the styles for the text color based on which shuttle's ETA is displayed (i.e. blue for West, green for East) to match the map

--- frontend/src/components/tvpanel.vue
+++ frontend/src/components/tvpanel.vue
@@ -3,7 +3,7 @@
     <Header />
     <Map />
     <div id="right-panel">
-      <ETAs />
+      <ETAs v-bind:eta-info="currentETAInfo" v-bind:show="shouldShowETAMessage"></ETAs>
       <News />
     </div>
   </div>
@@ -15,21 +15,97 @@ import Map from './tvpanel/Map.vue';
 import Header from './tvpanel/Header.vue';
 import News from './tvpanel/News.vue';
 import ETAs from './tvpanel/ETAs.vue';
-import Fusion from '@/fusion';
+import Fusion from '../fusion';
 import messagebox from './adminmessage.vue';
+import AdminMessageUpdate from '../structures/adminMessageUpdate';
+import UserLocationService from '../structures/userlocation.service';
+import { Stop, StopSVG } from '../structures/stop';
+import ETA from '../structures/eta';
+import Route from '../structures/route';
 export default Vue.extend({
   name: 'tvpanel',
   data() {
   return{
     fusion: new Fusion(),
+    currentETAInfo: null,
   }as {
       fusion: Fusion;
+     currentETAInfo: {} | null,
   };
 },
 mounted() {
   this.fusion.start();
 },
-  components: {
+  computed: {
+    message(): AdminMessageUpdate {
+        return this.$store.state.adminMessage;
+    },
+    busButtonActive(): boolean {
+      return this.$store.getters.getBusButtonVisible;
+    },
+    shouldShowETAMessage(): boolean {
+      return this.$store.state.settings.etasEnabled;
+    },
+    reconnecting(): boolean {
+      return this.$store.state.fusionConnected === false;
+    },
+  },
+  methods: {
+        updateETA() {
+      // find nearest stop
+      const pos = UserLocationService.getInstance().getCurrentLocation();
+      if (pos === undefined) {
+        this.currentETAInfo = null;
+        return;
+      }
+      const c = pos.coords as Coordinates;
+
+      let minDistance = Infinity;
+      let closestStop: Stop | null = null;
+      for (const stop of this.$store.state.Stops) {
+        const d = Math.hypot(c.longitude - stop.longitude, c.latitude - stop.latitude);
+        if (d < minDistance) {
+          minDistance = d;
+          closestStop = stop;
+        }
+      }
+      if (closestStop === null) {
+        this.currentETAInfo = null;
+        return;
+      }
+
+      // do we have an ETA for this stop? find the next soonest
+      let eta: ETA | null = null;
+      for (const e of this.$store.state.etas) {
+        if (e.stopID === closestStop.id) {
+          // is this the soonest?
+          if (eta === null || e.eta < eta.eta || e.arriving) {
+            eta = e;
+          }
+        }
+      }
+      if (eta === null) {
+        this.currentETAInfo = null;
+        return;
+      }
+
+      // get associated route
+      let route: Route | null = null;
+      for (const r of this.$store.state.Routes) {
+        if (r.id === eta.routeID) {
+          route = r;
+          break;
+        }
+      }
+      if (route === null) {
+        this.currentETAInfo = null;
+        return;
+      }
+
+      this.currentETAInfo = {eta, route, stop: closestStop};
+    },
+  },
+components: {
     Map,
     Header,
     News,
