9d2f355a55eb3152edc2105c2ee736108d0c0349
Alex Fontanellas <alexfontanellas@hotmail.com>
2019-11-12 16:28:13 -0500

    sorted routes

--- frontend/src/components/settings.vue
+++ frontend/src/components/settings.vue
@@ -24,7 +24,7 @@
         </thead>
         <tbody>
             <tr v-for="route in routes" :key="route.id" v-if="route.active">
-            <th><router-link :to='"/admin/routes/" + String(route.id) + "/"' >{{route.name}}</router-link></th>
+            <th class = "route_format">{{route.name}}</th>
             <td><b-switch v-bind:value="route.enabled" v-on:input="routeEnable(route)"></b-switch></td>
             </tr>
         </tbody>
@@ -71,7 +71,27 @@ export default Vue.extend({
       },
     },
     routes(): Route[] {
-        return this.$store.state.Routes;
+        function compare(a: Route, b: Route) {
+          if (a.enabled === true && b.enabled === false) {
+            return -1;
+          }
+          if (a.enabled === false && b.enabled === true) {
+            return 1;
+          } else {
+              if (a.name > b.name) {
+                return 1;
+              }
+              return -1;
+          }
+          return 0;
+        }
+
+        const routes_arr = [];
+        for (const stop of this.$store.state.Routes) {
+          routes_arr.push(stop);
+        }
+
+        return routes_arr.sort(compare);
     },
   },
   methods: {
@@ -88,5 +108,10 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .parent {
   padding: 20px;
+}
+
+.route_format {
+  color: red;
+  font-weight: bold;
 }
 </style>
