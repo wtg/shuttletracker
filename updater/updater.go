6edf4a1c107d689d3d80a2b0813e7ffa877fca11
Alan <joya2@rpi.edu>
2019-11-19 16:19:47 -0500

    started code to check if route should be active currently.

--- updater/updater.go
+++ updater/updater.go
@@ -160,6 +160,20 @@ func (u *Updater) update() {
 	}
 }
 
+/*
+// checks if a route should be active
+func (u *Updater) UpdateRoutes() (route *shuttletracker.Route, err error){
+	routes, err := u.ms.Routes()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, route := range routes {
+		if ()
+	}
+}
+*/
+
 // nolint: gocyclo
 func (u *Updater) handleVehicleData(vehicleData string) {
 	match := u.dataRegexp.FindAllStringSubmatch(vehicleData, -1)[0]
