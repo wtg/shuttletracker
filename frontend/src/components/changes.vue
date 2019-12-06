bea00e3d4e444c5a1a0410699ac42d48b9b533d0
Evan Lazaro <evanlazaro@gmail.com>
2019-12-05 21:40:49 -0500

    Finish changes page

--- frontend/src/components/changes.vue
+++ frontend/src/components/changes.vue
@@ -2,20 +2,47 @@
   <div class="parent content">
     <h2>Spring 2020 Route Changes</h2>
     <h3>The Changes</h3>
-    <p>Starting January 2020 the current East and West route system will be changed to include three routes: North, South, and West. An image of the new routes is shown below. The new routes can additionally be viewed on the Tracker by toggling them in the /settings page.</p>
+    <p>Starting January 2020 the current East and West route system will be changed to include three routes: North, South, and West. An image of the new routes is shown below. The new routes can additionally be viewed on the Tracker by toggling them in the <a href="/settings">/settings</a> page.</p>
 
     <p>All Shuttle hours will remain the same. These route changes will be occurring only apply for Weekdays, the current East and West routes will remain on Weekends.</p>
 
-    <h3>Purpose</h3>
-    <p>The overall goal of these changes was to reduce shuttle loop time, remove underutilized stops, and add stops where there is current demand. Some things to note about the changes:</p>
-    <ul>
-      <li>All routes will move clockwise</li>
-      <li>Blitman Commons stop is moving to the bus shelter on 6th Ave</li>
-      <li>The City Station stop is moving across the street to City Station East</li>
-      <li>North and South shuttles will both stop at B-lot and LXA</li>
-      <li>South and West shuttles will both stop at the Off Campus Commons</li>
-      <li>North and West shuttles will both stop at the new Ricketts stop</li>
-    </ul>
+    <p>The overall goal of these changes was to reduce shuttle loop time, remove underutilized stops, and add stops where there is current demand.</p>
+
+    <img src="https://i.imgur.com/EyQY1zH.jpg"/>
+    <br>
+    <div id="text">
+    <p><strong>West Route</strong>
+        <br>
+        <br>1. CBIS stop added by crosswalk
+        <br>• (20) 15th St and College Ave stop moved to South Route
+        <br>3. College St stop added by entrance to Academy Hall lot
+        <br>4. The City Station stop is moving across the street to City Station East by the parking garage entrance
+        <br>5. Blitman Commons stop is moving to the bus shelter on 6th Ave
+        <br>• (19) Polytech stop is moving to the east side of the street at the intersection of Congress and 15th, in front of Muza, will also be part of South Route
+        <br>7. Troy stop is being moved to the east end of Ricketts building
+        <br>
+    </p>
+    <p><strong>North Route</strong>
+        <br>
+        <br>8. All stops along Sage Ave are being merged to in front of E-Complex
+        <br>• Winslow stop is removed
+        <br>9. 9th St and Sage Ave will stop on 9th St
+        <br>10. Alumni House stop is moving across the street to in front of the Alumni House
+        <br>11. Beman Park stop added at 15th St and Jacob St
+        <br>12. Colonie stop moved to directly in front of Colonie A on street
+        <br>13. Brinsmade Terrace, Sunset Terrace 1, and Sunset Terrace 2 are unchanged
+        <br>14. E Lot stop is moving to the intersection of Cook Dr and Sunset Terrace
+        <br>15. B Lot stop added by bus shelter
+        <br>
+    </p>
+    <p><strong>South Route</strong>
+        <br>
+        <br>16. B Lot stop added by bus shelter
+        <br>17. LXA stop is moving to the southwest corner of the intersection of Cook Dr and Sunset Terrace
+        <br>18. Tibbits Ave stop is moving to intersection of Tibbits Ave and Orchard Ave
+        <br>19. Polytech stop added in front of Muza
+        <br>20. 15th St and College St stop added in front of Fire House</p>
+    </div>
 
     <h3>Credit</h3>
     <p>These changes were designed and proposed by the Facilities and Services Committee of the Student Senate in collaboration with Parking and Transportation. Thank you to all members of FSC for their work over the past several months and to Parking and Transportation for being extremely receptive to feedback and very willing to make these changes.</p>
@@ -34,6 +61,7 @@
       Cait Bennett<br>
       Neha Keshan<br>
       Noah Cohen<br>
+      Gretchen Forbush<br>
       Jakob Waryas<br>
     </p>
 
@@ -49,10 +77,21 @@ export default Vue.extend({});
 .parent {
   padding: 20px;
 }
+@media only screen and (min-width: 70em) {
+  .parent {
+    padding: 20px 20em;
+  }
+}
 p {
   margin-bottom: 1em;
 }
 .u {
   text-decoration: underline;
+}
+img{
+  width:90%;
+  max-width: 60em;
+  margin-left:auto;
+  margin-right:auto;
 }
 </style>
