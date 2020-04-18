d35df2a0a8ed8114ca193ef4e4a827babb2d4041
Matt Czyr <mattczyr@gmail.com>
2020-04-17 20:36:54 -0400

    Accept spoof data grouped into JSON files on a per-vehicle basis and document spoof manager functionality

--- spoofer/spoofer.go
+++ spoofer/spoofer.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
-	"regexp"
 	"strconv"
 	"sync"
 	"time"
@@ -14,19 +13,22 @@ import (
 	"github.com/wtg/shuttletracker/log"
 )
 
-// Updater handles periodically grabbing the latest vehicle location data from iTrak.
+// Spoofer creates "fake" (spoofed) updates at regular intervals from data in the spoof_data folder
 type Spoofer struct {
 	cfg           Config
 	spoofInterval time.Duration
 	SpoofUpdates  bool
 	spoofIndexes  map[int]int
-	dataRegexp    *regexp.Regexp
+	updates       map[int][]shuttletracker.Location
+	updateID      int64
 	ms            shuttletracker.ModelService
 	mutex         *sync.Mutex
 	sm            *sync.Mutex
 	subscribers   []func(*shuttletracker.Location)
 }
 
+// Configuration for Spoofer; determines whether or not spoofed updates will be created and the interval
+// at which it will occur
 type Config struct {
 	SpoofInterval string
 	SpoofUpdates  bool
@@ -47,15 +49,10 @@ func New(cfg Config, ms shuttletracker.ModelService) (*Spoofer, error) {
 		return nil, err
 	}
 	spoofer.spoofInterval = interval
-
 	spoofer.SpoofUpdates = cfg.SpoofUpdates
-
-	// Match each API field with any number (+)
-	//   of the previous expressions (\d digit, \. escaped period, - negative number)
-	//   Specify named capturing groups to store each field from data feed
-	spoofer.dataRegexp = regexp.MustCompile(`(?P<id>Vehicle ID:([\d\.]+)) (?P<lat>lat:([\d\.-]+)) (?P<lng>lon:([\d\.-]+)) (?P<heading>dir:([\d\.-]+)) (?P<speed>spd:([\d\.-]+)) (?P<lock>lck:([\d\.-]+)) (?P<time>time:([\d]+)) (?P<date>date:([\d]+)) (?P<status>trig:([\d]+))`)
-
 	spoofer.spoofIndexes = make(map[int]int)
+	spoofer.updates = make(map[int][]shuttletracker.Location)
+	spoofer.updateID = 1
 
 	return spoofer, nil
 }
@@ -76,58 +73,66 @@ func (s *Spoofer) Run() {
 		log.Debug("Spoofer started.")
 		ticker := time.Tick(s.spoofInterval)
 
-		// Do one initial spoof.
+		// Parse all update data
+		s.parseUpdates()
+
+		// Do one initial spoof
 		s.spoof()
 
-		// Spoof updates every spoofInterval
+		// Spoof updates for each vehicle every spoofInterval
 		for range ticker {
 			s.spoof()
 		}
 	}
 }
 
-// Subscribe allows callers to provide a function that is called after Updater parses a new Location.
-func (s *Spoofer) Subscribe(f func(*shuttletracker.Location)) {
-	s.sm.Lock()
-	s.subscribers = append(s.subscribers, f)
-	s.sm.Unlock()
-}
-
-func (s *Spoofer) notifySubscribers(loc *shuttletracker.Location) {
-	s.sm.Lock()
-	for _, sub := range s.subscribers {
-		go sub(loc)
-	}
-	s.sm.Unlock()
-}
-
-// Spoofs locations for each shuttle
-func (s *Spoofer) spoof() {
+// Sequentially reads and caches all JSON data to create updates from
+func (s *Spoofer) parseUpdates() {
 	wd, err := os.Getwd()
 	if err != nil {
 		log.WithError(err).Errorf("Error getting working directory")
 		return
 	}
 	vehicleIndex := 0
-	filenamePrefix := wd + "/spoof_data/update" + strconv.Itoa(s.spoofIndexes[vehicleIndex])
-	filename := filenamePrefix + "-vehicle" + strconv.Itoa(vehicleIndex) + ".json"
+	filename := wd + "/spoof_data/vehicle" + strconv.Itoa(vehicleIndex) + ".json"
 	_, err = os.Stat(filename)
 	for err == nil {
-		spooffile, err := os.Open(filename)
+		vehiclefile, err := os.Open(filename)
 		if err != nil {
-			log.WithError(err).Errorf("Error opening update %d file", s.spoofIndexes[vehicleIndex])
+			log.WithError(err).Errorf("Error opening vehicle %d file", vehicleIndex)
 			return
 		}
-		bytes, err := ioutil.ReadAll(spooffile)
+		bytes, err := ioutil.ReadAll(vehiclefile)
 		if err != nil {
-			log.WithError(err).Errorf("Error spoofing update %d", s.spoofIndexes[vehicleIndex])
+			log.WithError(err).Errorf("Error reading vehicle %d file", vehicleIndex)
 		}
-		var update shuttletracker.Location
-		json.Unmarshal(bytes, &update)
+		var updates []shuttletracker.Location
+		json.Unmarshal(bytes, &updates)
 
+		log.Debugf("Read %d updates for vehicle %d", len(updates), vehicleIndex)
+
+		// Only cache data for this vehicle if it has updates
+		if len(updates) > 0 {
+			s.updates[vehicleIndex] = updates
+			s.spoofIndexes[vehicleIndex] = 0
+		}
+
+		vehicleIndex += 1
+		filename = wd + "/spoof_data/vehicle" + strconv.Itoa(vehicleIndex) + ".json"
+		if _, err = os.Stat(filename); err != nil {
+			break
+		}
+	}
+}
+
+// Spoofs the next location for each vehicle
+func (s *Spoofer) spoof() {
+	for vehicleIndex, updates := range s.updates {
+		update := updates[s.spoofIndexes[vehicleIndex]]
 		update.Created = time.Now()
 		update.Time = time.Now()
-
+		update.TrackerID = "4572001148" // TODO
+		update.ID = s.updateID
 		if err := s.ms.CreateLocation(&update); err != nil {
 			log.WithError(err).Errorf("Could not create spoofed location")
 			return
@@ -137,15 +142,26 @@ func (s *Spoofer) spoof() {
 		s.notifySubscribers(&update)
 
 		s.spoofIndexes[vehicleIndex] += 1
-		spoofIndexTestFilename := wd + "/spoof_data/update" + strconv.Itoa(s.spoofIndexes[vehicleIndex]) + "-vehicle0.json"
-		if _, err = os.Stat(spoofIndexTestFilename); err != nil {
+		if s.spoofIndexes[vehicleIndex] >= len(updates) {
 			s.spoofIndexes[vehicleIndex] = 0
 		}
-
-		vehicleIndex += 1
-		filename = filenamePrefix + "-vehicle" + strconv.Itoa(vehicleIndex) + ".json"
-		if _, err = os.Stat(filename); err != nil {
-			break
-		}
+		s.updateID += 1
 	}
+}
+
+// Subscribe allows callers to provide a function that is called after Spoofer creates a new location.
+// Automatically reroutes all of Updater's subscribers to Spoofer when update spoofing is enabled.
+func (s *Spoofer) Subscribe(f func(*shuttletracker.Location)) {
+	s.sm.Lock()
+	s.subscribers = append(s.subscribers, f)
+	s.sm.Unlock()
+}
+
+// Notifies all of Spoofer's subscribers with a new location.
+func (s *Spoofer) notifySubscribers(loc *shuttletracker.Location) {
+	s.sm.Lock()
+	for _, sub := range s.subscribers {
+		go sub(loc)
+	}
+	s.sm.Unlock()
 }
