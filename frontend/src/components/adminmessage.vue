6a277e09a4aec0c672427e6286d710f24d21d024
Sean H <43757314+seandheffernan@users.noreply.github.com>
2020-08-17 22:40:12 -0700

    Dark theme update (#298)
    * Added colors and styling for leaflet panels for dark theme.
    
    * Added coloring for banners to change for Dark Theme.
    
    * Added watcher to adminMessage to display Bus Button when message is closed.

--- frontend/src/components/adminmessage.vue
+++ frontend/src/components/adminmessage.vue
@@ -23,6 +23,13 @@ export default Vue.extend({
             return this.$store.state.adminMessage;
         },
     },
+    watch: {
+      hide() {
+        if (this.hide) {
+          this.$store.state.adminMessage.enabled = false;
+        }
+      },
+    },
 });
 </script>
 
@@ -36,8 +43,8 @@ export default Vue.extend({
   left: 0;
   right: 0;
   padding: 10px 30px 10px 15px; /* leave some space for the close button */
-  background: #ee2222;
-  color: #ffffff;
+  background: var(--color-banner-background);
+  color: var(--color-banner-color);
   text-align: center;
   font-size: 15px;
 }
