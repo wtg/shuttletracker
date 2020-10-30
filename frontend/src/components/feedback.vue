5da955c06392cc96fb687f7e2e4c37bc55a6ae8e
pgravelin <pygravelin@gmail.com>
2020-10-29 21:02:49 -0400

    Admin feedback prompts are now visible; added the ability for users to submit forms

--- frontend/src/components/feedback.vue
+++ frontend/src/components/feedback.vue
@@ -1,38 +1,92 @@
 <template>
     <div class = "parent content">
         <h2>Feedback</h2>
-        <div v-if="(this.message !== undefined && this.message.enabled === true)" >
-            <div style="width: 100%;float:left;" v-html="this.message.message"></div>
+        <div v-if="this.adminMessage !== undefined" >
+            <div style="width: 100%;float:left;" v-html="this.adminMessage"></div>
         </div>
         <!-- <p>
             <br>We'd love to hear back from you!
             <br>If you have any comments, suggestions, or concerns, submit them down below and they'll be sent directly to us.
         </p> -->
-        <p>
+        <div>
             <td style="padding-top: 5px;">
             <textarea 
+                v-model="feedbackMessage"
                 placeholder="beep beep I'm a sheep . . ." 
                 rows = "8" cols = "127">
             </textarea>
             <br>
-            <button class="submit" type="submit" form="feedback" value="Submit"
+            <button @click="save" class="submit" type="submit" form="feedback" value="Submit"
             id="submit">Submit</button>
             </td>
-        </p>
+        </div>
+        <div v-if="fail" class="notification is-danger">
+            <p>Failed to submit form.</p>
+        </div>
+        <div v-if="success" class="notification is-success">
+            <p>Submission successful! Thanks for sending us your feedback!</p>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import InfoServiceProvider from '@/structures/serviceproviders/info.service';
+import AdminServiceProvider from '@/structures/serviceproviders/admin.service';
+import FeedbackMessageUpdate from '@/structures/feedbackMessageUpdate';
+import Form from '@/structures/form';
+import { setTimeout } from 'timers';
+
+const sp = new InfoServiceProvider();
+
 export default Vue.extend({
-    name: 'feedback',
     data() {
         return {
-            hide: false,
+            feedbackMessage: '',
+            adminMessage: '',
+            fail: false,
+            success: false,
+        }as {
+            feedbackMessage: string;
+            adminMessage: string;
+            fail: boolean;
+            success: boolean;
         };
+    },
+    mounted() {
+        this.getAdminMessage();
+    },
+    methods: {
+        save() {
+            const myMessage = new Form(-1, this.feedbackMessage, new Date(), false);
+            AdminServiceProvider.CreateForm(myMessage).then((resp) => {
+                if (resp.ok) {
+                    this.success = true;
+                    setTimeout(() => {
+                        this.success = false;
+                    }, 1000);
+                } else {
+                    this.fail = true;
+                    setTimeout(() => {
+                        this.fail = false;
+                    }, 1000);
+                }
+            }).catch(() => {
+                this.fail = true;
+                setTimeout(() => {
+                    this.fail = false;
+                }, 1000);
+            });
+        },
+        getAdminMessage() {
+            sp.GrabFeedbackMessage().then((message: FeedbackMessageUpdate) => {
+                this.adminMessage = message.message;
+            });
+        },
     },
 });
 </script>
+
 <style lang="scss" scoped>
 .parent{
     padding: 50px;
