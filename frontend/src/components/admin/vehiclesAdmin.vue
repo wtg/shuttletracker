30fa6addeea83e5cee947adfa527ff70df88ae76
Matt Czyr <mattczyr@gmail.com>
2021-03-18 20:51:00 -0400

    Allow vehicles to be imported from JSON through the admin panel

--- frontend/src/components/admin/vehiclesAdmin.vue
+++ frontend/src/components/admin/vehiclesAdmin.vue
@@ -26,6 +26,20 @@
                     <th></th>
                     <th><button @click="$router.push('/admin/vehicles/-1/new')" class="button is-success">New</button></th>
                 </tr>
+                <div class="file">
+                  <label class="file-label">
+                    <input class="file-input" type="file" name="import" @change="handleFileUpload">
+                    <span class="file-cta">
+                      <span class="file-icon">
+                        <img src="./../../assets/upload-icon.svg"></i>
+                      </span>
+                      <span class="file-label">
+                        Choose a file…
+                      </span>
+                    </span>
+                  </label>
+                  <button v-on:click="addImportedVehicles()" class="button is-success">Submit</button>
+                </div>
             </tbody>
         </table>
     </div>
@@ -47,9 +61,11 @@ export default Vue.extend({
         return{
             shouldDelete: false,
             vehicleToDelete: undefined,
+            file: null,
         } as {
             shouldDelete: boolean,
             vehicleToDelete: Vehicle | undefined,
+            file: any,
         };
     },
     components: {
@@ -66,6 +82,50 @@ export default Vue.extend({
                     this.$store.dispatch('grabVehicles');
                 });
             }
+        },
+
+        addImportedVehicles() {
+          const reader = new FileReader();
+          let json = null;
+          // Capture file information
+          reader.onload = ((theFile) => {
+            return (e: any) => {
+              try {
+                json = JSON.parse(e.target.result);
+                // If JSON.parse does not return a usable value, throw an Error
+                if (json.length === undefined || json.length === 0) {
+                  throw new Error('Improper JSON formatting');
+                }
+                for (let i = 0; i < json.length; i++) {
+                    const obj = json[i];
+                    // Create a new Vehicle object using the data in obj
+                    const newVehicle = new Vehicle(obj.id, obj.name, obj.created, obj.updated, obj.enabled, obj.tracker_id);
+                    // If creating the new Vehicle failed, the JSON file was not formatted correctly. Throw an Error
+                    if (!newVehicle) {
+                      throw new Error('Improper JSON formatting');
+                    }
+                    // Create the vehicle in the database
+                    AdminServiceProvider.NewVehicle(newVehicle).then(() => {
+                            this.$store.dispatch('grabVehicles');
+                    });
+                }
+              } catch (e) {
+                // If we get a SyntaxError, we have received a Non-JSON file. Replace the error to reflect this.
+                if (e instanceof SyntaxError) {
+                  e = new Error('Non-JSON file submitted');
+                }
+                // Display the Error to the user, and log it in the console
+                alert(e);
+                console.log(e);
+              }
+            };
+          })(this.file);
+          reader.readAsText(this.file);
+        },
+
+        // Gets the user-submitted file and stores it
+        handleFileUpload(event: any) {
+          this.file = event.target.files[0];
         },
     },
 });
